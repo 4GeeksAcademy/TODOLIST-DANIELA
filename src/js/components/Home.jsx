1ae97f0d39e3f32409521c24a5e67287ab8b83b0
danipadi13 <159074064+danipadi13@users.noreply.github.com>
2025-07-22 13:58:54 +0000

    Formulario de lista de tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from 'react';
+export default function ListaDePendientes () {
+    const [tareas, setTareas] = useState([]);
+    const [textoActual, setTextoActual]= useState("");
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            const contenido = textoActual.trim();
+            if (contenido) {
+                setTareas(prev => [...prev, {id: Date.now(), contenido }]);
+            }
+            setTextoActual("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+            }
+        };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+        const handleDelete =(id) => {
+            setTareas(prev => prev.filter(tarea => tarea.id !== id));
+        };
+    return(
+        <div className="contenedor-tareas">
+            <h1 className="titulo-principal">Mis tareas</h1>
+        <input
+        type="text"
+         className="entrada-tarea"
+        placeholder="Escribe la tarea y marca Enter"
+        value={textoActual}
+        onChange={(e) => setTextoActual(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+       {tareas.length === 0 ? (
+        <p className="mensaje-sin-tareas">Hoy no tienes tareas</p>
+      ) : (
+         <ul className="lista-tareas">
+          {tareas.map(tarea => (
+            <li key={tarea.id} className="item-tarea">
+                <span className="texto-tarea"> {tarea.contenido}</span>
+              <button
+              className="boton-eliminar"
+                onClick={() => eliminarTarea(tarea.id)}
+                aria-label="Eliminar tarea">
+        
+                </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 
-export default Home;
+}
+    
